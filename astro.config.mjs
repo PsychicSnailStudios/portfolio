7b4d3521e58ea14ba9ac64c500849d0c7636d560
PsychicSnailStudios <dawsonetheredge@outlook.com>
2022-04-07 14:04:59 -0500

    fix nav links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://dawson-e.netlify.app/',
+    site: 'https://dawson-e.netlify.app',
     vite: {
         ssr: {
             external: ["svgo"],
