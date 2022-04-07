95cb84f74e88aff946e33c06cf6e00189471620a
PsychicSnailStudios <dawsonetheredge@outlook.com>
2022-04-07 13:57:21 -0500

    astro compile fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://psychicsnailstudios.github.io',
-    base: '/portfolio',
+    site: 'https://dawson-e.netlify.app/',
     vite: {
         ssr: {
             external: ["svgo"],
