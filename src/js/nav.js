22fa927139076150bab098e1f33c7f84f91d6b5c
PsychicSnailStudios <dawsonetheredge@outlook.com>
2022-04-19 18:29:26 -0500

    update nav highlight

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,20 +1,11 @@
 const navBtn = document.querySelector('#menu-btn');
-const navBtns = document.querySelectorAll('.nav-button');
 
 navBtn.addEventListener('click', () => {
     navBtn.setAttribute('aria-expanded', navBtn.getAttribute('aria-expanded') === 'false' ? 'true' : 'false');
 })
 
-setNavigation();
-
-function setNavigation() {
-    var path = window.location.pathname;
-    
-    navBtns.forEach(nb => {
-        var href = nb.getAttribute('href');
-        
-        if (path === href) {
-            nb.classList.add('active');
-        }
-    });
-}
+document.querySelectorAll('.nav-button').forEach(link => {
+    if (link.href === window.location.href) {
+        link.setAttribute('aria-current', 'page')
+    }
+})
