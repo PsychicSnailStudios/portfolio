d8a7736c791e03e86d0da3bb5311a043a7734db9
PsychicSnailStudios <dawsonetheredge@outlook.com>
2022-06-10 20:53:10 -0500

    fix nav and fonts

--- src/js/nav.js
+++ src/js/nav.js
@@ -8,4 +8,7 @@ document.querySelectorAll('.nav-button').forEach(link => {
     if (link.href === window.location.href) {
         link.setAttribute('aria-current', 'page')
     }
+    else {
+        link.setAttribute('aria-current', 'false')
+    }
 })
